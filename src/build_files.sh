ad2312dfefe9105affeba5f8d747d41879f03666
Christopher <christopher172003@outlook.cl>
2024-05-16 16:10:41 -0400

    .

--- src/build_files.sh
+++ src/build_files.sh
@@ -1,10 +1,10 @@
 # build_files.sh
 echo "Deployement started..."
 
-python3 -m venv venv
 python3 -V
 
 source venv/bin/activate
+python3 -m venv venv
 pip3 install --upgrade pip --quiet
 pip3 install -r requirements.txt --quiet
 python3 manage.py collectstatic
