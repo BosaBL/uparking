c29cbd41ebe955eaf0758d73a1f0dcf780f2d7da
Christopher <christopher172003@outlook.cl>
2024-05-17 19:31:54 -0400

    Se arregla comando de build

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,7 @@ source venv/bin/activate
 python3 -V
 pip3 install --upgrade pip --quiet
 pip3 install -r requirements.txt --quiet
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 
 
 echo "Deployement finished..."
