a5aaa86b05cf82f24189efa6dfa34f160a96e75c
Christopher Sepúlveda <43054246+BosaBL@users.noreply.github.com>
2024-06-02 03:21:08 -0400

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,9 +6,9 @@ python3 -V
 pip3 install --upgrade pip --quiet
 pip3 install -r requirements.txt --quiet
 
-python manage.py makemigrations
-python manage.py makemigrations --merge
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py makemigrations --merge
+python3 manage.py migrate
 
 python3 manage.py collectstatic --noinput
 
