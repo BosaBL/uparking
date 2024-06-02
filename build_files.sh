6418cab38b435654c42931e00a6c62fc6e233e6f
Christopher Sepúlveda <43054246+BosaBL@users.noreply.github.com>
2024-06-02 03:17:33 -0400

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,7 @@
 #/bin/sh
 
-ls -a
-
-cd ..
-
-ls -a
-
 echo "Deployement started..."
 
-python3 -m venv venv
-
-source venv/bin/activate
 python3 -V
 pip3 install --upgrade pip --quiet
 pip3 install -r requirements.txt --quiet
