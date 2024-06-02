20fdf96c0fb9a6b6c71dadb720bd79b1d458468c
Christopher Sepúlveda <43054246+BosaBL@users.noreply.github.com>
2024-06-02 03:39:49 -0400

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,27 +1,11 @@
 #/bin/sh
 
-gdalinfo --version
-
-ls -a
-
-cd ..
-
-ls -a
-
-cd ..
-
-ls -a
-
-cd ..
-
-ls -a
-
-
 echo "Deployement started..."
 
 python3 -V
 pip3 install --upgrade pip --quiet
 pip3 install -r requirements.txt --quiet
+pip3 install gdal
 
 python3 manage.py makemigrations
 python3 manage.py makemigrations --merge
